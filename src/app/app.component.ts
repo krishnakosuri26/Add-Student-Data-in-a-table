ee9c5476ccb0326eb38eda097dfd8279a13e59c9
krishnakosuri26 <157450361+krishnakosuri26@users.noreply.github.com>
2024-03-06 21:08:44 +0530

    student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'third';
+
+  studentName: string = "";
+  studentRollNo: number = 0;
+  studentYear: number = 0;
+  studentContact: number = 0;
+  studentActivity: string = "";
+  isDataSubmitted: boolean = false;
+  students: any = [];
+
+  display() {
+    this.students.push({
+      name: this.studentName,
+      rollNo: this.studentRollNo,
+      year: this.studentYear,
+      contact: this.studentContact,
+      activity: this.studentActivity
+    });
+
+    this.studentName = "";
+    this.studentRollNo = 0;
+    this.studentYear = 0;
+    this.studentContact = 0;
+    this.studentActivity = "";
+
+    this.isDataSubmitted = true;
+  }
+
+  erase(){
+    this.studentName = "";
+    this.studentRollNo = 0;
+    this.studentYear = 0;
+    this.studentContact = 0;
+    this.studentActivity = "";
+  }
+
+
+
 }
